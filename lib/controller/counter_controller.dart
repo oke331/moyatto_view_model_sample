035bbfec7967fc51b96fec21206f5500ee7ac66a
oke <takeo.takeo331@gmail.com>
2022-02-02 18:42:47 +0900

    Add counter controller

---
+++ lib/controller/counter_controller.dart
@@ -0,0 +1,31 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:minimal_riverpod_sample/repository/counter_repository.dart';
+import 'package:minimal_riverpod_sample/resorces/model/counter.dart';
+
+final counterController = StateNotifierProvider(
+  (ref) => CounterController(repository: ref.watch(counterRepository)),
+);
+
+class CounterController extends StateNotifier<AsyncValue<Counter>> {
+  CounterController({required this.repository})
+      : super(const AsyncValue.loading()) {
+    fetchCount();
+  }
+
+  final InMemoryCounterRepository repository;
+
+  Future<void> fetchCount() async {
+    state = const AsyncLoading();
+    try {
+      final result = await repository.fetchCount();
+      state = AsyncData(result);
+    } on Exception catch (e) {
+      state = AsyncError(e);
+    }
+  }
+
+  Future<void> registerCount({required int count}) async {
+    repository.registerCount(count: count);
+    AsyncData(count);
+  }
+}
